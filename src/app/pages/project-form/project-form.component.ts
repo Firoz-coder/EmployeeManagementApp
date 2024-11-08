9b56609e737441da706f8c354e0caf2d52e3161a
Firoz Shaikh <firoz.md@eviden.com>
2024-11-08 10:44:42 +0530

    project form component design completed

---
+++ src/app/pages/project-form/project-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-project-form',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './project-form.component.html',
+  styleUrl: './project-form.component.css'
+})
+export class ProjectFormComponent {
+
+}
